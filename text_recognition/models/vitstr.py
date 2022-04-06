6ff118e7954c3548b0231af3a146c4abe126cb01
Dmitry Indenbom <dmitry.indenbom@yandex.ru>
2022-04-06 21:59:53 +0300

    Minor refactoring

--- text_recognition/models/vitstr.py
+++ text_recognition/models/vitstr.py
@@ -53,10 +53,10 @@ class ViTSTR(VisionTransformer):
         self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
     def forward_features(self, x):
-        B = x.shape[0]
+        b = x.shape[0]
         x = self.patch_embed(x)
 
-        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        cls_tokens = self.cls_token.expand(b, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
         x = torch.cat((cls_tokens, x), dim=1)
         x = x + self.pos_embed
         x = self.pos_drop(x)
