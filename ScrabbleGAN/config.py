ScrabbleGAN Multi GPU training, fixes to gradient balancing and gradient clipping.

ScrabbleGAN/config.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 
 
@@ -8,23 +10,29 @@ class Config:
     char_w = 16
     channels = 3
 
-    batch_size = 16
-    num_epochs = 60
+    batch_scale = 2
+    batch_size = 8 * batch_scale
+    num_epochs = 1000
     epochs_lr_decay = 100  # learning rate decay will be applied for last these many steps (should be <= num_epochs)
 
     train_gen_steps = 3  # generator weights to be updated after every specified number of steps
-    grad_alpha = 1
+
+    grad_alpha = 2.
+
     grad_balance = True
+    min_grad_scale = 1e-6
+    max_grad_scale = 1.
 
     architecture = 'ScrabbleGAN'
 
     # Generator and Discriminator networks
     bn_linear = 'SN'
+    g_ch = 32
     g_shared = False
 
-    g_lr = 2e-4
-    d_lr = 2e-4
-    r_lr = 2e-4
+    g_lr = 5e-5 * batch_scale
+    d_lr = 5e-5 * batch_scale
+    r_lr = 5e-5 * batch_scale
     g_betas = [0., 0.999]
     d_betas = [0., 0.999]
     r_betas = [0., 0.999]
@@ -35,4 +43,8 @@ class Config:
     # Noise vector
     z_dim = 128
 
+    max_grad_norm = 10.
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    worker_count = os.cpu_count() - 1

ScrabbleGAN/data_loader/data_generator.py
@@ -53,11 +53,15 @@ class DataLoader:
         self.config = config
         self.pickle_path = pickle_path
 
-    def create_train_loader(self):
+    def create_train_loader(self, rank, workd_size, worker_count):
         self.dataset = CustomDataset(self.config, self.pickle_path)
+        if workd_size == 1:
+            sampler = torch.utils.data.RandomSampler(self.dataset)
+        else:
+            sampler = torch.utils.data.DistributedSampler(self.dataset, num_replicas=workd_size, rank=rank)
         return torch.utils.data.DataLoader(
-            self.dataset, batch_size=self.config.batch_size, shuffle=True,
-            num_workers=os.cpu_count() - 1, pin_memory=True, collate_fn=self.batch_collate)
+            self.dataset, batch_size=self.config.batch_size, sampler=sampler,
+            num_workers=worker_count, pin_memory=True, collate_fn=self.batch_collate, persistent_workers=True)
 
     def batch_collate(self, batch):
         items = {}

ScrabbleGAN/gan_models/ScrabbleGAN.py
@@ -97,11 +97,10 @@ class ScrabbleGAN(nn.Module):
         self.num_chars = len(char_map)
         self.word_map = WordMap(char_map)
 
-        self.batch_size = cfg.batch_size
         self.config = cfg
 
         self.R = Recognizer(cfg, self.num_chars)
-        self.G = BGAN.Generator(resolution=cfg.img_h, G_shared=cfg.g_shared,
+        self.G = BGAN.Generator(resolution=cfg.img_h, G_ch=cfg.g_ch, G_shared=cfg.g_shared,
                                 bn_linear=cfg.bn_linear, n_classes=self.num_chars, hier=True, channels=cfg.channels)
         self.D = BGAN.Discriminator(resolution=cfg.img_h, bn_linear=cfg.bn_linear, n_classes=self.num_chars, channels=cfg.channels)
 

ScrabbleGAN/train.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+import random
 import shutil
 import sys
 from importlib import import_module
@@ -18,50 +19,84 @@ from gan_utils.data_utils import *
 from gan_utils.training_utils import ModelCheckpoint
 from losses_and_metrics import loss_functions
 
-seed = 0
-torch.manual_seed(seed)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed(seed)
-np.random.seed(seed)
-
 level = logging.INFO
-format_log = '%(message)s'
+format_log = f"[{os.getpid():04d}] %(asctime)s %(levelname).1s: %(message)s"
 
-os.makedirs('output', exist_ok=True)
-handlers = [logging.FileHandler('output/output.log', mode='w', encoding='utf-8'),
+os.makedirs('outputs', exist_ok=True)
+handlers = [logging.FileHandler('outputs/output.log', mode='w', encoding='utf-8'),
             logging.StreamHandler(stream=sys.stdout)]
 logging.basicConfig(level=level, format=format_log, handlers=handlers)
 
 
-def get_train_loader(config, data_pkl_path):
+def init_seed(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def get_train_loader(config, data_pkl_path, rank, world_size, worker_count):
     data_loader = DataLoader(config, data_pkl_path)
-    data_loader = data_loader.create_train_loader()
+    data_loader = data_loader.create_train_loader(rank, world_size, max(round(worker_count / world_size), 1))
     return data_loader
 
 
 class Trainer:
-    def __init__(self, config, args):
+    def __init__(self, config, args, rank: int, local_rank: int, world_size: int):
         self.config = config
+
+        self.local_rank = int(local_rank)
+        self.rank = int(rank)
+        self.world_size = int(world_size)
+        assert 0 <= self.local_rank <= self.rank < self.world_size
+
         self.terminal_width = shutil.get_terminal_size((80, 20)).columns
-        logging.info(f' Loading Data '.center(self.terminal_width, '*'))
+        if self.local_rank == 0:
+            logging.info(f' Loading Data '.center(self.terminal_width, '*'))
 
         self.is_unlabeled_data = False
         if args.unlabeled_pkl_path:
             self.is_unlabeled_data = True
-            logging.info("Using unlabeled data to train discriminator")
+            if self.local_rank == 0:
+                logging.info("Using unlabeled data to train discriminator")
 
-        self.labeled_loader = get_train_loader(config, args.data_pkl_path)
+        self.labeled_loader = get_train_loader(config, args.data_pkl_path,
+                                               self.rank, self.world_size,
+                                               self.config.worker_count)
         self.num_chars = len(self.labeled_loader.dataset.char_map)
         if self.is_unlabeled_data:
-            self.unlabeled_loader = get_train_loader(config, args.unlabeled_pkl_path)
+            self.unlabeled_loader = get_train_loader(config, args.unlabeled_pkl_path,
+                                                     self.rank, self.world_size,
+                                                     self.config.worker_count)
 
         # Model
-        logging.info(f' Model: {self.config.architecture} '.center(self.terminal_width, '*'))
+        if self.local_rank == 0:
+            logging.info(f' Model: {self.config.architecture} '.center(self.terminal_width, '*'))
         model_type = import_module('gan_models.' + self.config.architecture)
         create_model = getattr(model_type, 'create_model')
         self.model = create_model(self.config, self.labeled_loader.dataset.char_map, args.lexicon_path)
-        logging.info(f'{self.model}\n')
-        self.model.to(self.config.device)
+
+        if self.world_size > 1:
+            from torch.nn.parallel import DistributedDataParallel
+            from torch.nn import SyncBatchNorm
+            self.model = SyncBatchNorm.convert_sync_batchnorm(self.model)
+            self.model.R = DistributedDataParallel(self.model.R,
+                                                   # find_unused_parameters=True,
+                                                   device_ids=[self.local_rank],
+                                                   output_device=self.local_rank)
+            self.model.G = DistributedDataParallel(self.model.G,
+                                                   # find_unused_parameters=True,
+                                                   device_ids=[self.local_rank],
+                                                   output_device=self.local_rank)
+            self.model.D = DistributedDataParallel(self.model.D,
+                                                   find_unused_parameters=True,
+                                                   device_ids=[self.local_rank],
+                                                   output_device=self.local_rank)
+        else:
+            self.model.to(self.config.device)
+        if self.local_rank == 0:
+            logging.info(f'{self.model}\n')
 
         self.word_map = WordMap(self.labeled_loader.dataset.char_map)
 
@@ -69,10 +104,14 @@ class Trainer:
         self.G_criterion = getattr(loss_functions, self.config.g_loss_fn)('G')
         self.D_criterion = getattr(loss_functions, self.config.d_loss_fn)('D')
         self.R_criterion = getattr(loss_functions, self.config.r_loss_fn)()
-        self.G_optimizer = torch.optim.Adam(self.model.G.parameters(), lr=self.config.g_lr, betas=self.config.g_betas)
-        self.D_optimizer = torch.optim.Adam(self.model.D.parameters(), lr=self.config.d_lr, betas=self.config.d_betas)
-        self.R_optimizer = torch.optim.Adam(self.model.R.parameters(), lr=self.config.r_lr, betas=self.config.r_betas)
+        self.G_optimizer = torch.optim.Adam(self.model.G.parameters(),
+                                            lr=self.config.g_lr * self.world_size, betas=self.config.g_betas)
+        self.D_optimizer = torch.optim.Adam(self.model.D.parameters(),
+                                            lr=self.config.d_lr * self.world_size, betas=self.config.d_betas)
+        self.R_optimizer = torch.optim.Adam(self.model.R.parameters(),
+                                            lr=self.config.r_lr * self.world_size, betas=self.config.r_betas)
         self.optimizers = [self.G_optimizer, self.D_optimizer, self.R_optimizer]
+        self.max_grad_norm = dict()
 
         # Use a linear learning rate decay but start the decay only after specified number of epochs
         def lr_decay_lambda(epoch):
@@ -82,7 +121,7 @@ class Trainer:
 
         self.start_epoch = 1
         # Load checkpoint if training is to be resumed
-        self.model_checkpoint = ModelCheckpoint(config=self.config)
+        self.model_checkpoint = ModelCheckpoint(config=self.config, weight_dir="./outputs/weights")
         if args.pretrain_path:
             self.model, self.optimizers, self.schedulers, self.start_epoch = \
                 self.model_checkpoint.load(
@@ -128,35 +167,36 @@ class Trainer:
         # the below part has been mostly copied from -
         # https://github.com/amzn/convolutional-handwriting-gan/blob/2cfbc794cca299445e5ba070c8634b6cd1a84261/models
         # /ScrabbleGAN_baseModel.py#L345
-        self.loss_G_total = self.loss_G + self.config.grad_alpha * self.loss_R_fake
-        grad_fake_R = torch.autograd.grad(self.loss_R_fake, self.model.fake_img, retain_graph=True)[0]
-        self.loss_grad_fake_R = 10 ** 6 * torch.mean(grad_fake_R ** 2)
-        grad_fake_adv = torch.autograd.grad(self.loss_G, self.model.fake_img, retain_graph=True)[0]
-        self.loss_grad_fake_adv = 10 ** 6 * torch.mean(grad_fake_adv ** 2)
-        if self.config.grad_balance:
-            epsilon = 10e-50
-            self.loss_G_total.backward(retain_graph=True)
-            grad_fake_R = torch.autograd.grad(self.loss_R_fake, self.model.fake_img,
-                                              create_graph=True, retain_graph=True)[0]
-            grad_fake_adv = torch.autograd.grad(self.loss_G, self.model.fake_img,
-                                                create_graph=True, retain_graph=True)[0]
-            a = self.config.grad_alpha * torch.div(torch.std(grad_fake_adv), epsilon + torch.std(grad_fake_R))
-            self.loss_R_fake = a.detach() * self.loss_R_fake
-            self.loss_G_total = self.loss_G + self.loss_R_fake
-            self.loss_G_total.backward(retain_graph=True)
-            grad_fake_R = torch.autograd.grad(self.loss_R_fake, self.model.fake_img,
-                                              create_graph=False, retain_graph=True)[0]
-            grad_fake_adv = torch.autograd.grad(self.loss_G, self.model.fake_img,
-                                                create_graph=False, retain_graph=True)[0]
-            self.loss_grad_fake_R = 10 ** 6 * torch.mean(grad_fake_R ** 2)
-            self.loss_grad_fake_adv = 10 ** 6 * torch.mean(grad_fake_adv ** 2)
-            with torch.no_grad():
-                self.loss_G_total.backward()
+
+        if hasattr(self.model.G, "no_sync"):
+            with self.model.G.no_sync():
+                self._balance_G_loss()
         else:
+            self._balance_G_loss()
+
+        with torch.no_grad():
             self.loss_G_total.backward()
 
+        self._clip_gradients('G', self.model.G)
         self.G_optimizer.step()
         self.G_optimizer.zero_grad()
+
+    def _balance_G_loss(self):
+        grad_fake_R = torch.autograd.grad(self.loss_R_fake, self.model.fake_img,
+                                          create_graph=False, retain_graph=True)[0].detach()
+        grad_fake_adv = torch.autograd.grad(self.loss_G, self.model.fake_img,
+                                            create_graph=False, retain_graph=True)[0].detach()
+        if self.config.grad_balance:
+            epsilon = 1e-24
+            scale = torch.sqrt(torch.sum(grad_fake_adv ** 2) / (epsilon + torch.sum(grad_fake_R ** 2)))
+            scale = torch.clip(scale, min=self.config.min_grad_scale, max=self.config.max_grad_scale)
+        else:
+            scale = 1
+
+        self.loss_grad_fake_R = 10 ** 6 * torch.mean(grad_fake_R ** 2)
+        self.loss_grad_fake_adv = 10 ** 6 * torch.mean(grad_fake_adv ** 2)
+
+        self.loss_G_total = self.loss_G + self.config.grad_alpha * scale * self.loss_R_fake
 
     def optimize_D_unlabeled(self):
         """Completes forward, backward, and optimize for D on unlabeled data"""
@@ -172,6 +212,8 @@ class Trainer:
         self.loss_D = self.loss_D_fake + self.loss_D_real
 
         self.loss_D.backward()
+
+        self._clip_gradients('D', self.model.D)
         self.D_optimizer.step()
         self.D_optimizer.zero_grad()
 
@@ -201,10 +243,16 @@ class Trainer:
 
         self.loss_D_and_R.backward()
 
+        self._clip_gradients('D', self.model.D)
+        self._clip_gradients('R', self.model.R)
         self.D_optimizer.step()
         self.R_optimizer.step()
         self.D_optimizer.zero_grad()
         self.R_optimizer.zero_grad()
+
+    def _clip_gradients(self, name, model):
+        total_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=self.config.max_grad_norm)
+        self.max_grad_norm[name] = max(self.max_grad_norm.get(name, 0.), float(total_norm))
 
     def train(self):
         logging.info(f' Training '.center(self.terminal_width, '*'))
@@ -212,6 +260,16 @@ class Trainer:
         for epoch in range(self.start_epoch, self.config.num_epochs + 1):
             logging.info(f' Epoch [{epoch}/{self.config.num_epochs}] '.center(self.terminal_width, 'x'))
             self.model.train()
+
+            if self.world_size > 1 and epoch > self.start_epoch:
+                import torch.distributed
+                for p in self.model.parameters():
+                    torch.distributed.broadcast(p, 0)
+
+                for o in self.optimizers:
+                    for _, v in sorted(o.state_dict()['state'].items()):
+                        if isinstance(v, torch.Tensor):
+                            torch.distributed.broadcast(v, 0)
 
             train_loader = self.labeled_loader
             len_loader = len(self.labeled_loader)
@@ -221,7 +279,7 @@ class Trainer:
                 else:
                     train_loader = zip(self.labeled_loader, cycle(self.unlabeled_loader))
 
-            progbar = tqdm(train_loader, total=len_loader)
+            progbar = tqdm(train_loader, total=len_loader, disable=None)
             losses_G, losses_D, losses_D_real, losses_D_fake = [], [], [], []
             losses_R_real, losses_R_fake, grads_fake_R, grads_fake_adv = [], [], [], []
             for i, batch_items in enumerate(progbar):
@@ -260,19 +318,26 @@ class Trainer:
                                          np.mean(losses_R_real), np.mean(losses_R_fake)))
 
             logging.info(f'G = {np.mean(losses_G):.3f}, D = {np.mean(losses_D):.3f}, '
-                         f'R_real = {np.mean(losses_R_real):.3f}, R_fake = {np.mean(losses_R_fake):.3f}'
-                         )
+                         f'R_real = {np.mean(losses_R_real):.3f}, R_fake = {np.mean(losses_R_fake):.3f}')
+            logging.info(f'grad_fake_R = {np.mean(grads_fake_R):.4g}, grad_G = {np.mean(grads_fake_adv):.4g}')
+            if self.local_rank == 0:
+                logging.info(f'Max grad norm: ' +
+                             ', '.join(f'{n} = {v:.4g}' for n, v in sorted(self.max_grad_norm.items())))
+            self.max_grad_norm.clear()
+
             # Save one generated fake image from last batch
-            img = self.model.fake_img.cpu().data.numpy()[0]
-            normalized_img = ((img + 1) * 255 / 2).astype(np.uint8)
-            normalized_img = np.moveaxis(normalized_img, 0, -1)
-            cv2.imwrite(f'./output/epoch_{epoch}_fake_img.png', normalized_img)
+            if self.local_rank == 0:
+                img = self.model.fake_img.cpu().data.numpy()[0]
+                normalized_img = ((img + 1) * 255 / 2).astype(np.uint8)
+                normalized_img = np.moveaxis(normalized_img, 0, -1)
+                os.makedirs('./outputs/images', exist_ok=True)
+                cv2.imwrite(f'./outputs/images/epoch_{epoch}_fake_img.png', normalized_img)
 
             # Print Recognizer prediction for 4 (or batch size) real images from last batch
             num_imgs = 4 if self.config.batch_size >= 4 else self.config.batch_size
             labels = self.word_map.decode(self.real_y[:num_imgs].cpu().numpy())
             preds = self.word_map.recognizer_decode(self.pred_R_real.max(2)[1].permute(1, 0)[:num_imgs].cpu().numpy())
-            logging.info('\nRecognizer predictions for real images:')
+            logging.info('Recognizer predictions for real images:')
             max_len_label = max([len(i) for i in labels])
             for lab, pred in zip(labels, preds):
                 logging.info(f'Actual: {lab:<{max_len_label + 2}}|  Predicted: {pred}')
@@ -291,7 +356,7 @@ class Trainer:
                 sch.step()
 
             # save checkpoint after every 5 epochs
-            if epoch % 5 == 0:
+            if epoch % 5 == 0 and self.local_rank == 0:
                 self.model_checkpoint.save(self.model, epoch, self.G_optimizer, self.D_optimizer, self.R_optimizer,
                                            *self.schedulers)
 
@@ -307,7 +372,23 @@ class Trainer:
         self.writer.close()
 
 
-if __name__ == "__main__":
+def setup_distributed_rank(local_rank: int):
+    import torch.distributed
+    print(f"Initializing NCCL local_rank={local_rank}")
+    torch.cuda.set_device(local_rank)
+    torch.distributed.init_process_group("nccl")
+    rank = torch.distributed.get_rank()
+    world_size = torch.distributed.get_world_size()
+    print(f"Using {torch.distributed.get_backend()}, rank={rank}, world_size={world_size}")
+    return rank, world_size
+
+
+def cleanup_distributed_rank():
+    import torch.distributed
+    torch.distributed.destroy_process_group()
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_pkl_path", required=True, type=str,
                         help="Path to the pickle processed data")
@@ -320,7 +401,24 @@ if __name__ == "__main__":
     parser.add_argument("--lexicon_path", action='append', required=True,
                         type=str, help="Path to the lexicon txt. Can be passed "
                                        "multiple times")
+    parser.add_argument("--distributed", action="store_true", help="run in distributed job")
     args = parser.parse_args()
 
-    trainer = Trainer(Config, args)
-    trainer.train()
+    if args.distributed:
+        local_rank = int(os.environ['LOCAL_RANK'])
+        rank, world_size = setup_distributed_rank(local_rank)
+    else:
+        local_rank, rank, world_size = 0, 0, 1
+
+    init_seed(local_rank)
+
+    try:
+        trainer = Trainer(Config, args, local_rank, rank, world_size)
+        trainer.train()
+    finally:
+        if args.distributed:
+            cleanup_distributed_rank()
+
+
+if __name__ == "__main__":
+    main()